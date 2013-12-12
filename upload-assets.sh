21fb471fa1a6989566338ded74202350d3b1d204
Nikolay Sokolov <chemikadze@gmail.com>
2013-12-12 15:36:19 +0400

    Upload manifests to s3 too

--- upload-assets.sh
+++ upload-assets.sh
@@ -11,4 +11,8 @@ fi
 
 s3cmd put -P -m application/x-gzip target/nxlog.tar.gz s3://qubell-logging/$VERSION/nxlog.tar.gz
 s3cmd put -P -m application/x-gzip target/logstash.tar.gz s3://qubell-logging/$VERSION/logstash.tar.gz
+
 s3cmd put -P -m application/javascript target/kibana-all.js s3://qubell-logging/$VERSION/kibana-all.js
+
+s3cmd put -P -m application/x-yaml manifests/logstash.yaml s3://qubell-logging/$VERSION/logstash.yaml
+s3cmd put -P -m application/x-yaml manifests/nxlog-example.yaml s3://qubell-logging/$VERSION/nxlog-example.yaml
