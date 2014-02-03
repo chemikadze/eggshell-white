004b66acb331c77d752a234de9898e53982b97af
Nikolay Sokolov <chemikadze@gmail.com>
2014-02-03 19:41:33 +0400

    Fix kibana version

--- cookbooks/logstash/recipes/kibana.rb
+++ cookbooks/logstash/recipes/kibana.rb
@@ -14,13 +14,14 @@ bash "install kibana" do
   user "root"
   group "root"
   code <<-EOC
-    tar -xzf #{Chef::Config[:file_cache_path]}/kibana.tar.gz -C /usr/share/nginx/html
+    mkdir -p /usr/share/nginx/html/kibana-master
+    tar -xzf #{Chef::Config[:file_cache_path]}/kibana.tar.gz -C /usr/share/nginx/html/kibana-master --strip-components=1
   EOC
   action :nothing
   notifies :create, "template[/usr/share/nginx/html/kibana-master/src/app/dashboards/logstash.json]", :immediately
 end
 
 remote_file "#{Chef::Config[:file_cache_path]}/kibana.tar.gz" do
-  source "https://github.com/elasticsearch/kibana/archive/master.tar.gz"
+  source "https://github.com/elasticsearch/kibana/archive/9d6573e3d8130d722f71835957fcdf602ca1e18f.tar.gz"
   notifies :run, "bash[install kibana]", :immediately
 end
