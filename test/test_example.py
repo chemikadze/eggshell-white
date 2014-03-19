f72b64e24c9488bf2a2eb9fde401942ee1099caa
Nikolay Sokolov <chemikadze@gmail.com>
2014-03-19 15:59:27 +0400

    Use latest version of cookbooks in tests

--- test/test_example.py
+++ test/test_example.py
@@ -1,7 +1,7 @@
 import os
 
 from test_runner import BaseComponentTestCase
-from qubell.api.private.testing import instance, environment, workflow, values
+from qubell.api.private.testing import environment
 
 
 @environment({
@@ -11,8 +11,15 @@ class ComponentTestCase(BaseComponentTestCase):
     name = "eggshell-white"
     apps = [{
         "name": name,
-        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name))
+        "file": os.path.realpath(os.path.join(os.path.dirname(__file__), '../%s.yml' % name)),
+        "parameters": {
+            "configuration.logging-cookbooks-version": "latest"
+        }
     }]
+
+    @classmethod
+    def timeout(cls):
+        return 20;
 
     def test_pass(self):
         assert True, "Just another test, that passes"
